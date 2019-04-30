b75cfd1c2e5a1a0ef85d679f33b4ed61dc8edc61
mkusaka <hinoshita1992@gmail.com>
2019-04-30 12:24:20 +0900

    feat: io action sample main sample

---
+++ books_example/io_action.hs
@@ -0,0 +1,8 @@
+main :: IO ()
+-- main = -- error pattern
+--     putStrLn "Hello, world"
+--     putStrLn "fom first hs file"
+
+main = do
+    putStrLn "Hello, world"
+    putStrLn "fom first hs file"
---
+++ books_example/main.hs
@@ -0,0 +1,8 @@
+module Main (main) where
+  import System.Environment
+  main :: IO ()
+  main = do
+    let title = "Current User:"
+    user <- getEnv "USER"
+    putStrLn title
+    putStrLn user
