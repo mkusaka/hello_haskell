9392fae97d58ada665a327f2a1466752250e237f
mkusaka <hinoshita1992@gmail.com>
2019-05-29 00:47:15 +0900

    fundamental modules

---
+++ workthrough/11.modules/Main.hs
@@ -0,0 +1,7 @@
+import Geometry
+
+main = do
+  print $ width (Rect 3 4)
+  print $ height (Rect 3 4)
+  print $ area (Tri 3 4)
+  print $ area (Rect 3 4)
---
+++ workthrough/11.modules/datalist.hs
@@ -0,0 +1,5 @@
+import Data.List
+
+main = do
+  print $ sort [2, 1, 3]
+  print $ Data.List.sort [2, 1, 3]
